020b5518701ea8eaa11eb7d14f057f6523c786c9
matealex <alexandru_matei2000@yahoo.com>
2016-01-16 16:10:37 +0200

    Security

--- HR/HR-war/src/java/servlets/PositionListServlet.java
+++ HR/HR-war/src/java/servlets/PositionListServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/positions/")
+@WebServlet("/positions")
 //@WebServlet(name = "PositionServlet", urlPatterns = {"/positions/"})
 public class PositionListServlet extends HttpServlet {
 
